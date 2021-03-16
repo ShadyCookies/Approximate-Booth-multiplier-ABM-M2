aabc74ed79f5aa79a877cd0acd1d6a07dfd20a13
Naren Vilva <51068682+ShadyCookies@users.noreply.github.com>
2021-03-16 22:04:16 +0530

    Update binvec2dec_signed.m

--- binvec2dec_signed.m
+++ binvec2dec_signed.m
@@ -1,11 +1,11 @@
-function out = binvec2dec( bin )
-    len = length(bin);
-    if( bin(len) )
-        out = -2^(len-1);
-    else
-        out = 0;
-    end
-    for i=1:len-1
-            out = out + bin(i)*2^(i-1);
-    end
+function out = binvec2dec_signed( bin )
+len = length(bin);
+if( bin(len) )
+    out = -2^(len-1);
+else
+    out = 0;
+end
+for i=1:len-1
+    out = out + bin(i)*2^(i-1);
+end
 end
